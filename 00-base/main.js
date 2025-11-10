ed4e976cfd9b61683a1ce90e1f28f303ae4be22a
Luca Lambia <luca@lambia.it>
2025-11-10 10:20:40 +0100

    first commit

---
+++ 00-base/main.js
@@ -0,0 +1,29 @@
+console.log("Hello JS World!");
+
+// document.writeln("Hello JS World");
+// document.writeln("<p>prova</p>");
+
+alert("Benvenuto in JS!")
+
+document.getElementById("msg").innerHTML = "Pagina caricata correttamente!";
+
+
+// let nome = "Luca \"Lambda\" Lambiase";
+// let nome = 'Luca "Lambda" Lambiase';
+
+
+let nickname = "Lambda";
+// let nomeConcatenato = 'Luca "' + nickname + '" Lambiase';
+// console.log(nomeConcatenato);
+
+let nomeCompleto = `Luca "${nickname}" Lambiase`;
+console.log(nomeCompleto);
+
+const a = 3;
+const b = 39;
+
+const somma = a + b;
+
+console.log("La risposta definitiva è: ", somma);
+console.log("Booleano: ", true);
+console.log("Stringa: ", "true");
